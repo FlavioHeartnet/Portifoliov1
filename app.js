8c633d337ede9b5d44940f7c97cfe36fd4865c1b
Flavio Nogueira <flavionogueirabarros@gmail.com>
2021-02-08 09:06:51 -0300

    estrutura para deploy

--- app.js
+++ app.js
@@ -8,6 +8,6 @@ const parcel_bundler_1 = __importDefault(require("parcel-bundler"));
 const path_1 = __importDefault(require("path"));
 const app = express_1.default();
 const port = 3000;
-const bundler = new parcel_bundler_1.default(path_1.default.join(__dirname, "../src/dist/index.html"));
+const bundler = new parcel_bundler_1.default(path_1.default.join(__dirname, "../src/dist/public/index.html"));
 app.use(bundler.middleware());
 app.listen(port, () => console.log(`{rodando na porta http://localhost:${port}/)`));
